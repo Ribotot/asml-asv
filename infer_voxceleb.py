3b1d1fce47846442d6df9e4f866e753167a4563e
Ribotot <brent1104@gmail.com>
2023-12-11 16:55:09 +0900

    Baseline of speaker verification

---
+++ infer_voxceleb.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import sys, time, os, argparse
+import yaml
+import numpy
+import torch
+import glob
+import zipfile
+import warnings
+import datetime
+from tuneThreshold import *
+from SpeakerNet import *
+from dataloader_voxceleb import *
+warnings.simplefilter("ignore")
+
+## ===== ===== ===== ===== ===== ===== ===== =====
+## Parse arguments
+## ===== ===== ===== ===== ===== ===== ===== =====
+
+parser = argparse.ArgumentParser(description = "SpeakerNet")
+
+parser.add_argument('--config',         type=str,   default=None,   help='Config YAML file')
+
+## Data loader
+parser.add_argument('--max_frames',     type=int,   default=200,    help='Input length to the network for training')
+parser.add_argument('--batch_size',     type=int,   default=200,    help='Batch size, number of speakers per batch')
+parser.add_argument('--max_seg_per_spk', type=int,  default=500,    help='Maximum number of utterances per speaker per epoch')
+parser.add_argument('--nDataLoaderThread', type=int, default=5,     help='Number of loader threads')
+parser.add_argument('--augment',        type=bool,  default=False,  help='Augment input')
+parser.add_argument('--seed',           type=int,   default=10,     help='Seed for the random number generator')
+
+## Training details
+parser.add_argument('--test_interval',  type=int,   default=10,     help='Test and save every [test_interval] epochs')
+parser.add_argument('--max_epoch',      type=int,   default=500,    help='Maximum number of epochs')
+parser.add_argument('--trainfunc',      type=str,   default="",     help='Loss function')
+
+## Optimizer
+parser.add_argument('--optimizer',      type=str,   default="adam", help='sgd or adam')
+parser.add_argument('--scheduler',      type=str,   default="steplr", help='Learning rate scheduler')
+parser.add_argument('--lr',             type=float, default=0.001,  help='Learning rate')
+parser.add_argument("--lr_decay",       type=float, default=0.95,   help='Learning rate decay every [test_interval] epochs')
+parser.add_argument('--weight_decay',   type=float, default=0,      help='Weight decay in the optimizer')
+
+## Loss functions
+parser.add_argument("--hard_prob",      type=float, default=0.5,    help='Hard negative mining probability, otherwise random, only for some loss functions')
+parser.add_argument("--hard_rank",      type=int,   default=10,     help='Hard negative mining rank in the batch, only for some loss functions')
+parser.add_argument('--margin',         type=float, default=0.1,    help='Loss margin, only for some loss functions')
+parser.add_argument('--scale',          type=float, default=30,     help='Loss scale, only for some loss functions')
+parser.add_argument('--nPerSpeaker',    type=int,   default=1,      help='Number of utterances per speaker per batch, only for metric learning based losses')
+parser.add_argument('--nClasses',       type=int,   default=5994,   help='Number of speakers in the softmax layer, only for softmax-based losses')
+
+## Evaluation parameters
+parser.add_argument('--dcf_p_target',   type=float, default=0.01,   help='A priori probability of the specified target speaker')
+parser.add_argument('--dcf_c_miss',     type=float, default=1,      help='Cost of a missed detection')
+parser.add_argument('--dcf_c_fa',       type=float, default=1,      help='Cost of a spurious detection')
+
+## Load and save
+parser.add_argument('--initial_model',  type=str,   default="",     help='Initial model weights')
+parser.add_argument('--save_path',      type=str,   default="exps/exp1", help='Path for model and logs')
+
+## For test only
+## Test data
+parser.add_argument('--test_list',      type=str,   default="data/test_list.txt",   help='Evaluation list')
+parser.add_argument('--test_path',      type=str,   default="data/voxceleb1", help='Absolute path to the test set')
+
+## Model definition
+parser.add_argument('--n_mels',         type=int,   default=40,     help='Number of mel filterbanks')
+parser.add_argument('--log_input',      type=bool,  default=False,  help='Log input features')
+parser.add_argument('--model',          type=str,   default="",     help='Name of model definition')
+parser.add_argument('--encoder_type',   type=str,   default="SAP",  help='Type of encoder')
+parser.add_argument('--nOut',           type=int,   default=512,    help='Embedding size in the last FC layer')
+parser.add_argument('--sinc_stride',    type=int,   default=10,    help='Stride size of the first analytic filterbank layer of RawNet3')
+
+args = parser.parse_args()
+
+## Parse YAML
+def find_option_type(key, parser):
+    for opt in parser._get_optional_actions():
+        if ('--' + key) in opt.option_strings:
+           return opt.type
+    raise ValueError
+
+if args.config is not None:
+    with open(args.config, "r") as f:
+        yml_config = yaml.load(f, Loader=yaml.FullLoader)
+    for k, v in yml_config.items():
+        if k in args.__dict__:
+            typ = find_option_type(k, parser)
+            args.__dict__[k] = typ(v)
+        else:
+            sys.stderr.write("Ignored unknown parameter {} in yaml.\n".format(k))
+
+
+## ===== ===== ===== ===== ===== ===== ===== =====
+## Trainer script
+## ===== ===== ===== ===== ===== ===== ===== =====
+
+def main_worker(gpu, ngpus_per_node, args):
+
+
+    ## Load models
+    s = SpeakerNet(**vars(args))
+
+    s = WrappedModel(s).cuda(gpu)
+
+    it = 1
+
+    ## Write args to scorefile
+    scorefile   = open(args.result_save_path+"/scores.txt", "a+")
+
+    trainer     = ModelTrainer(s, **vars(args))
+
+    ## Load model weights
+    modelfiles = glob.glob('%s/model0*.model'%args.model_save_path)
+    modelfiles.sort()
+
+    if(args.initial_model != ""):
+        trainer.loadParameters(args.initial_model)
+        print("Model {} loaded!".format(args.initial_model))
+    elif len(modelfiles) >= 1:
+        trainer.loadParameters(modelfiles[-1])
+        print("Model {} loaded from previous state!".format(modelfiles[-1]))
+        it = int(os.path.splitext(os.path.basename(modelfiles[-1]))[0][5:]) + 1
+
+    ## Evaluation code - must run on single GPU
+
+    pytorch_total_params = sum(p.numel() for p in s.module.__S__.parameters())
+
+    print('Total parameters: ',pytorch_total_params)
+    print('Test list',args.test_list)
+    
+    sc, lab, _ = trainer.evaluateFromList(**vars(args))
+
+    result = tuneThresholdfromScore(sc, lab, [1, 0.1])
+
+    fnrs, fprs, thresholds = ComputeErrorRates(sc, lab)
+    mindcf, threshold = ComputeMinDcf(fnrs, fprs, thresholds, args.dcf_p_target, args.dcf_c_miss, args.dcf_c_fa)
+
+    print('\n',time.strftime("%Y-%m-%d %H:%M:%S"), "VEER {:2.4f}".format(result[1]), "MinDCF {:2.5f}".format(mindcf))
+
+
+## ===== ===== ===== ===== ===== ===== ===== =====
+## Main function
+## ===== ===== ===== ===== ===== ===== ===== =====
+
+
+def main():
+    args.model_save_path     = args.save_path+"/model"
+    args.result_save_path    = args.save_path+"/result"
+    args.feat_save_path      = ""
+
+    os.makedirs(args.model_save_path, exist_ok=True)
+    os.makedirs(args.result_save_path, exist_ok=True)
+
+    print('Python Version:', sys.version)
+    print('PyTorch Version:', torch.__version__)
+    print('Number of GPUs:', torch.cuda.device_count())
+    print('Save path:',args.save_path)
+
+    gpu = args.gpu
+
+    main_worker(gpu, None, args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/custom/ECAPATDNN512.py
@@ -0,0 +1,203 @@
+# repo:repo:lawlict/ECAPA-TDNN 
+# repo:TaoRuijie/Loss-Gated-Learning 
+
+# revised
+# Copyright 2023 Choi Jeong-Hwan
+
+
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchaudio
+
+class SEModule(nn.Module):
+    def __init__(self, channels, bottleneck=128):
+        super(SEModule, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool1d(1),
+            nn.Conv1d(channels, bottleneck, kernel_size=1, padding=0),
+            nn.ReLU(),
+            nn.BatchNorm1d(bottleneck),
+            nn.Conv1d(bottleneck, channels, kernel_size=1, padding=0),
+            nn.Sigmoid(),
+            )
+
+    def forward(self, input):
+        x = self.se(input)
+        return input * x
+
+class Bottle2neck(nn.Module):
+    def __init__(self, inplanes, planes, kernel_size=None, dilation=None, scale = 8):
+        super(Bottle2neck, self).__init__()
+
+        width       = int(math.floor(planes / scale))
+        self.conv1  = nn.Conv1d(inplanes, width*scale, kernel_size=1)
+        self.bn1    = nn.BatchNorm1d(width*scale)
+        self.nums   = scale -1
+        convs       = []
+        bns         = []
+        num_pad = math.floor(kernel_size/2)*dilation
+        for i in range(self.nums):
+            convs.append(nn.Conv1d(width, width, kernel_size=kernel_size, dilation=dilation, padding=num_pad))
+            bns.append(nn.BatchNorm1d(width))
+        self.convs  = nn.ModuleList(convs)
+        self.bns    = nn.ModuleList(bns)
+        self.conv3  = nn.Conv1d(width*scale, planes, kernel_size=1)
+        self.bn3    = nn.BatchNorm1d(planes)
+        self.relu   = nn.ReLU()
+        self.width  = width
+        self.se     = SEModule(planes)
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.relu(out)
+        out = self.bn1(out)
+        spx = torch.split(out, self.width, 1)
+        for i in range(self.nums):
+          if i==0:
+            sp = spx[i]
+          else:
+            sp = sp + spx[i]
+          sp = self.convs[i](sp)
+          sp = self.relu(sp)
+          sp = self.bns[i](sp)
+          if i==0:
+            out = sp
+          else:
+            out = torch.cat((out, sp), 1)
+        out = torch.cat((out, spx[self.nums]),1)
+        out = self.conv3(out)
+        out = self.relu(out)
+        out = self.bn3(out)
+        out = self.se(out)
+        out += residual
+
+        return out 
+
+class PreEmphasis(torch.nn.Module):
+
+    def __init__(self, coef: float = 0.97):
+        super().__init__()
+        self.coef = coef
+        self.register_buffer(
+            'flipped_filter', torch.FloatTensor([-self.coef, 1.]).unsqueeze(0).unsqueeze(0)
+        )
+
+    def forward(self, input: torch.tensor) -> torch.tensor:
+        input = input.unsqueeze(1)
+        input = F.pad(input, (1, 0), 'reflect')
+        return F.conv1d(input, self.flipped_filter).squeeze(1)
+
+class ECAPA_TDNN(nn.Module): # Here we use a small ECAPA-TDNN, C=512. In my experiences, C=1024 slightly improves the performance but need more training time.
+    def __init__(self, C = 512, nOut = 256, n_mels = 80, **kwargs):
+        super(ECAPA_TDNN, self).__init__()
+        self.conv1  = nn.Conv1d(n_mels, C, kernel_size=5, stride=1, padding=2)
+        self.relu   = nn.ReLU()
+        self.bn1    = nn.BatchNorm1d(C)
+        self.layer1 = Bottle2neck(C, C, kernel_size=3, dilation=2, scale=8)
+        self.layer2 = Bottle2neck(C, C, kernel_size=3, dilation=3, scale=8)
+        self.layer3 = Bottle2neck(C, C, kernel_size=3, dilation=4, scale=8)
+        self.layer4 = nn.Conv1d(3*C, 1536, kernel_size=1)
+        self.torchfbank = torch.nn.Sequential(
+            PreEmphasis(),
+            torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_fft=512, win_length=400, hop_length=160, f_min = 20, f_max = 7600, window_fn=torch.hamming_window, n_mels=n_mels),
+            )
+        self.attention = nn.Sequential(
+            nn.Conv1d(4608, 256, kernel_size=1),
+            nn.ReLU(),
+            nn.BatchNorm1d(256),
+            nn.Tanh(),
+            nn.Conv1d(256, 1536, kernel_size=1),
+            nn.Softmax(dim=2),
+            )
+        self.bn5 = nn.BatchNorm1d(3072)
+        self.fc6 = nn.Linear(3072, nOut)
+        self.bn6 = nn.BatchNorm1d(nOut)
+
+
+    def _before_pooling(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.bn1(x)
+        x1 = self.layer1(x)
+        x2 = self.layer2(x+x1)
+        x3 = self.layer3(x+x1+x2)
+        x = self.layer4(torch.cat((x1,x2,x3),dim=1))
+        x = self.relu(x)
+        return x
+
+    def _before_penultimate(self, x):
+        t = x.size()[-1]
+        global_x = torch.cat((x,torch.mean(x,dim=2,keepdim=True).repeat(1,1,t), torch.sqrt(torch.var(x,dim=2,keepdim=True).clamp(min=1e-4)).repeat(1,1,t)), dim=1)
+        w = self.attention(global_x)
+        mu = torch.sum(x * w, dim=2)
+        sg = torch.sqrt( ( torch.sum((x**2) * w, dim=2) - mu**2 ).clamp(min=1e-4) )
+        x = torch.cat((mu,sg),1)
+        x = self.bn5(x)
+        x = self.fc6(x)
+        x = self.bn6(x)
+        return x
+
+    def wave2feat(self, x, max_frame=False):
+        with torch.no_grad():
+            with torch.cuda.amp.autocast(enabled=False):
+                if max_frame:
+                    audiosize = x.shape[1]
+                    max_audio = max_frame*160 +240 # 16kHz defalt
+                    if audiosize <= max_audio:
+                        import math
+                        shortage    = math.floor( ( max_audio - audiosize + 1 ) / 2 )
+                        x = F.pad(x, (shortage,shortage), "constant", 0)          
+                x = self.torchfbank(x)+1e-6
+                x = x.log()
+                x = x - torch.mean(x, dim=-1, keepdim=True)
+                x = x[:,:,1:-1]
+                x = x.detach()
+        return x
+
+    def wave2emb(self, wave, max_frame=False):
+        feat = self.wave2feat(wave, max_frame)
+        late_feat = self._before_pooling(feat)
+        emb = self._before_penultimate(late_feat)
+        return emb
+
+
+    def forward(self, x, max_frame=False):
+        x = self.wave2emb(x, max_frame)
+        return x
+
+if __name__=="__main__":
+    # batch_size, num_frames, feat_dim = 1, 3000, 80
+    batch_size, second = 1, 4
+    x = torch.randn(batch_size, int(second*16000))
+    # x_tp = x.transpose(1, 2)
+
+    model = ECAPA_TDNN(nOut = 256)
+
+    model.eval()
+
+    torch.set_num_threads(1)
+    import timeit
+    model.eval()
+    number = 10
+    end_start = timeit.timeit(stmt='model(x)', globals=globals(), number=number)
+    print('CPU Time :',end_start/number*1000, 'ms') 
+    # exit()
+
+    model.eval()
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+    start.record()
+    embs = model(x)
+    end.record()
+    torch.cuda.synchronize()
+    print('GPU Time :',start.elapsed_time(end), 'ms')
+    # exit()
+
+    from fvcore.nn import FlopCountAnalysis
+    # self.forward = self.entire_wave2emb ## for use FlopCountAnalysis (Comment out def forward)
+    model.eval()
+    flops = FlopCountAnalysis(model, x)
+    print(flops.total())
