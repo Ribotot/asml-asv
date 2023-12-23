2c00b285105cf8fb370baa541f23f89a904d649d
Ribotot <126688844+Ribotot@users.noreply.github.com>
2023-12-23 15:22:47 +0900

    Add files via upload

--- scheduler/onecyclelr.py
+++ scheduler/onecyclelr.py
@@ -2,13 +2,15 @@
 # -*- encoding: utf-8 -*-
 
 import torch
+import math
 
-def Scheduler(optimizer, max_epoch, lr, warmup_epoch, **kwargs):
+def Scheduler(optimizer, batch_size, max_epoch, lr, warmup_epoch, epoch_per_sample, **kwargs):
 
+	total_iteration = math.ceil(epoch_per_sample/batch_size)*max_epoch
 	pct_start = float(warmup_epoch)/float(max_epoch)
-	sche_fn = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=lr, pct_start=pct_start, anneal_strategy='cos')
-	lr_step = 'epoch'
+	sche_fn = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=lr, total_steps=total_iteration, pct_start=pct_start, anneal_strategy='cos')
+	lr_step = 'iteration'
 
-	print('Initialised step LR scheduler')
+	print('Initialised OneCycle LR scheduler')
 
 	return sche_fn, lr_step
