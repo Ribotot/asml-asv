2528ace4ebeb10fe448008111dc33c8616b57747
brent1104@gmail.com <brent1104@gmail.com>
2023-12-26 13:40:16 +0900

    Modify voxceleb train code

--- train_voxceleb.py
+++ train_voxceleb.py
@@ -12,10 +12,7 @@ import datetime
 from tuneThreshold import *
 from SpeakerNet import *
 from dataloader_voxceleb import *
-<<<<<<< HEAD
-=======
 from utils import *
->>>>>>> 463ada6aeb053540ce2428831b625449a57c7a09
 import torch.distributed as dist
 import torch.multiprocessing as mp
 warnings.simplefilter("ignore")
@@ -30,33 +27,6 @@ parser.add_argument('--config',         type=str,   default=None,   help='Config
 
 ## Data loader
 parser.add_argument('--max_frames',     type=int,   default=200,    help='Input length to the network for training')
-<<<<<<< HEAD
-parser.add_argument('--batch_size',     type=int,   default=200,    help='Batch size, number of speakers per batch')
-parser.add_argument('--max_seg_per_spk', type=int,  default=500,    help='Maximum number of utterances per speaker per epoch')
-parser.add_argument('--nDataLoaderThread', type=int, default=5,     help='Number of loader threads')
-parser.add_argument('--augment',        type=bool,  default=False,  help='Augment input')
-parser.add_argument('--seed',           type=int,   default=10,     help='Seed for the random number generator')
-
-## Training details
-parser.add_argument('--test_interval',  type=int,   default=10,     help='Test and save every [test_interval] epochs')
-parser.add_argument('--max_epoch',      type=int,   default=500,    help='Maximum number of epochs')
-parser.add_argument('--trainfunc',      type=str,   default="",     help='Loss function')
-
-## Optimizer
-parser.add_argument('--optimizer',      type=str,   default="adam", help='sgd or adam')
-parser.add_argument('--scheduler',      type=str,   default="steplr", help='Learning rate scheduler')
-parser.add_argument('--lr',             type=float, default=0.001,  help='Learning rate')
-parser.add_argument("--lr_decay",       type=float, default=0.95,   help='Learning rate decay every [test_interval] epochs')
-parser.add_argument('--weight_decay',   type=float, default=0,      help='Weight decay in the optimizer')
-
-## Loss functions
-parser.add_argument("--hard_prob",      type=float, default=0.5,    help='Hard negative mining probability, otherwise random, only for some loss functions')
-parser.add_argument("--hard_rank",      type=int,   default=10,     help='Hard negative mining rank in the batch, only for some loss functions')
-parser.add_argument('--margin',         type=float, default=0.1,    help='Loss margin, only for some loss functions')
-parser.add_argument('--scale',          type=float, default=30,     help='Loss scale, only for some loss functions')
-parser.add_argument('--nPerSpeaker',    type=int,   default=1,      help='Number of utterances per speaker per batch, only for metric learning based losses')
-parser.add_argument('--nClasses',       type=int,   default=5994,   help='Number of speakers in the softmax layer, only for softmax-based losses')
-=======
 parser.add_argument('--eval_frames',    type=str,   default=None,   help='Input length to the network for testing None uses the whole files')
 parser.add_argument('--batch_size',     type=int,   default=200,    help='Batch size, number of speakers per batch')
 parser.add_argument('--max_seg_per_spk', type=int,  default=500,    help='Maximum number of utterances per speaker per epoch')
@@ -88,7 +58,6 @@ parser.add_argument('--nPerSpeaker',    type=int,   default=1,      help='Number
 parser.add_argument('--nClasses',       type=int,   default=5994,   help='Number of speakers in the softmax layer, only for softmax-based losses')
 parser.add_argument("--hard_prob",      type=float, default=0.5,    help='Hard negative mining probability, otherwise random, only for [triplet] loss functions')
 parser.add_argument("--hard_rank",      type=int,   default=10,     help='Hard negative mining rank in the batch, only for [triplet] loss functions')
->>>>>>> 463ada6aeb053540ce2428831b625449a57c7a09
 
 ## Evaluation parameters
 parser.add_argument('--dcf_p_target',   type=float, default=0.01,   help='A priori probability of the specified target speaker')
@@ -100,14 +69,6 @@ parser.add_argument('--initial_model',  type=str,   default="",     help='Initia
 parser.add_argument('--save_path',      type=str,   default="exps/exp1", help='Path for model and logs')
 
 ## Training and test data
-<<<<<<< HEAD
-parser.add_argument('--train_list',     type=str,   default="data/train_list.txt",  help='Train list')
-parser.add_argument('--test_list',      type=str,   default="data/test_list.txt",   help='Evaluation list')
-parser.add_argument('--train_path',     type=str,   default="data/voxceleb2", help='Absolute path to the train set')
-parser.add_argument('--test_path',      type=str,   default="data/voxceleb1", help='Absolute path to the test set')
-parser.add_argument('--musan_path',     type=str,   default="data/musan_split", help='Absolute path to the test set')
-parser.add_argument('--rir_path',       type=str,   default="data/RIRS_NOISES/simulated_rirs", help='Absolute path to the test set')
-=======
 # parser.add_argument('--train_list',     type=str,   default="data/train_list.txt",  help='Train list')
 # parser.add_argument('--test_list',      type=str,   default="data/test_list.txt",   help='Evaluation list')
 # parser.add_argument('--train_path',     type=str,   default="data/voxceleb2", help='Absolute path to the train set')
@@ -122,7 +83,6 @@ parser.add_argument('--train_path',        type=str,   default="",          help
 parser.add_argument('--test_path',         type=str,   default="/media/jh2/f22b587f-8065-4c02-9b74-f6b9f5a89581/DB/VoxCeleb1/test/wav/", help='Absolute path to the test set');
 parser.add_argument('--musan_path',        type=str,   default="/media/jh2/f22b587f-8065-4c02-9b74-f6b9f5a89581/DB/musan_split", help='Absolute path to the test set');
 parser.add_argument('--rir_path',          type=str,   default="/media/jh2/f22b587f-8065-4c02-9b74-f6b9f5a89581/DB/RIRS_NOISES/simulated_rirs", help='Absolute path to the test set');
->>>>>>> 463ada6aeb053540ce2428831b625449a57c7a09
 
 ## Model definition
 parser.add_argument('--n_mels',         type=int,   default=40,     help='Number of mel filterbanks')
@@ -132,11 +92,6 @@ parser.add_argument('--encoder_type',   type=str,   default="SAP",  help='Type o
 parser.add_argument('--nOut',           type=int,   default=512,    help='Embedding size in the last FC layer')
 parser.add_argument('--sinc_stride',    type=int,   default=10,    help='Stride size of the first analytic filterbank layer of RawNet3')
 
-<<<<<<< HEAD
-## For test only
-parser.add_argument('--eval',           dest='eval', action='store_true', help='Eval only')
-=======
->>>>>>> 463ada6aeb053540ce2428831b625449a57c7a09
 
 ## Distributed and mixed precision training
 parser.add_argument('--port',           type=str,   default="8888", help='Port for distributed training, input as text')
@@ -196,10 +151,7 @@ def main_worker(gpu, ngpus_per_node, args):
     if args.gpu == 0:
         ## Write args to scorefile
         scorefile   = open(args.result_save_path+"/scores.txt", "a+")
-<<<<<<< HEAD
-=======
         dict2scp(args.result_save_path+"/args.scp", vars(args))
->>>>>>> 463ada6aeb053540ce2428831b625449a57c7a09
 
     ## Initialise trainer and data loader
     train_dataset = train_dataset_loader(**vars(args))
@@ -216,12 +168,9 @@ def main_worker(gpu, ngpus_per_node, args):
         drop_last=True,
     )
 
-<<<<<<< HEAD
-=======
     # iter(train_sampler)
     args.epoch_per_sample = len(train_dataset) # if we use "len(train_sampler)", it might be cause minor error for scheduler
     
->>>>>>> 463ada6aeb053540ce2428831b625449a57c7a09
     trainer     = ModelTrainer(s, **vars(args))
 
     ## Load model weights
@@ -296,10 +245,6 @@ def main_worker(gpu, ngpus_per_node, args):
 ## Main function
 ## ===== ===== ===== ===== ===== ===== ===== =====
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 463ada6aeb053540ce2428831b625449a57c7a09
 def main():
     args.model_save_path     = args.save_path+"/model"
     args.result_save_path    = args.save_path+"/result"
