ac466304b98afd16a4c97b00774aac2891edf048
brent1104@gmail.com <brent1104@gmail.com>
2024-01-12 07:57:22 +0900

    Update the train codes

---
+++ dataloader_train_all.py
@@ -0,0 +1,338 @@
+#! /usr/bin/python
+# -*- encoding: utf-8 -*-
+
+import torch
+import numpy
+import random
+import pdb
+import os
+import threading
+import time
+import math
+import glob
+import soundfile
+from scipy import signal
+from scipy.io import wavfile
+from torch.utils.data import Dataset, DataLoader
+import torch.distributed as dist
+
+def round_down(num, divisor):
+    return num - (num%divisor)
+
+def worker_init_fn(worker_id):
+    numpy.random.seed(numpy.random.get_state()[1][0] + worker_id)
+
+
+def loadWAV(filename, max_frames=None):
+    # Read wav file and convert to torch tensor
+    audio, sample_rate = soundfile.read(filename)
+
+    audiosize = audio.shape[0]
+
+    # Maximum audio length
+    if max_frames == None:
+        max_audio = audiosize
+
+        feats = []
+        feats.append(audio)
+
+    else:
+        max_audio = int(max_frames) * 160 + 240
+
+        if audiosize <= max_audio:
+            shortage    = max_audio - audiosize + 1 
+            audio       = numpy.pad(audio, (0, shortage), 'wrap')
+            audiosize   = audio.shape[0]
+
+        startframe = numpy.array([numpy.int64(random.random()*(audiosize-max_audio))])
+        
+        feats = []
+        for asf in startframe:
+            feats.append(audio[int(asf):int(asf)+max_audio])
+
+    feat = numpy.stack(feats,axis=0).astype(numpy.float64)
+
+    return feat;
+    
+class AugmentWAV(object):
+
+    def __init__(self, musan_path, rir_path, max_frames):
+
+        self.max_frames = max_frames
+        self.max_audio  = max_audio = max_frames * 160 + 240
+
+        self.noisetypes = ['noise','speech','music']
+
+        self.noisesnr   = {'noise':[0,15],'speech':[13,20],'music':[5,15]}
+        self.numnoise   = {'noise':[1,1], 'speech':[3,7],  'music':[1,1] }
+        self.noiselist  = {}
+
+        augment_files   = glob.glob(os.path.join(musan_path,'*/*/*/*.wav'));
+
+        for file in augment_files:
+            if not file.split('/')[-4] in self.noiselist:
+                self.noiselist[file.split('/')[-4]] = []
+            self.noiselist[file.split('/')[-4]].append(file)
+
+        self.rir_files  = glob.glob(os.path.join(rir_path,'*/*/*.wav'));
+
+    def additive_noise(self, noisecat, audio):
+
+        clean_db = 10 * numpy.log10(numpy.mean(audio ** 2)+1e-4) 
+
+        numnoise    = self.numnoise[noisecat]
+        noiselist   = random.sample(self.noiselist[noisecat], random.randint(numnoise[0],numnoise[1]))
+
+        noises = []
+
+        for noise in noiselist:
+
+            noiseaudio  = loadWAV(noise, self.max_frames)
+            noise_snr   = random.uniform(self.noisesnr[noisecat][0],self.noisesnr[noisecat][1])
+            noise_db = 10 * numpy.log10(numpy.mean(noiseaudio[0] ** 2)+1e-4) 
+            noises.append(numpy.sqrt(10 ** ((clean_db - noise_db - noise_snr) / 10)) * noiseaudio)
+
+        return numpy.sum(numpy.concatenate(noises,axis=0),axis=0,keepdims=True) + audio
+
+    def reverberate(self, audio):
+
+        rir_file    = random.choice(self.rir_files)
+        
+        rir, fs     = soundfile.read(rir_file)
+        rir         = numpy.expand_dims(rir.astype(numpy.float64),0)
+        rir         = rir / numpy.sqrt(numpy.sum(rir**2))
+
+        return signal.convolve(audio, rir, mode='full')[:,:self.max_audio]
+
+
+def make_voxceleb_train_dict(voxceleb_train_list, voxceleb_train_path):
+    # Read training files
+    with open(voxceleb_train_list) as dataset_file:
+        lines = dataset_file.readlines();
+
+    train_dict = {}    
+    for line in lines:
+        # spk_wavfile = line.strip().split();
+        spkutt_wavfile = line.strip().split(); ## if you extract utterances based on kaldi (scp file)
+
+        # spk = spk_wavfile[0]
+        spk = spkutt_wavfile[0].split('-')[0] ## if you extract utterances based on kaldi (scp file)
+
+        # filename = os.path.join(voxceleb_train_path,spk_wavfile[-1]);
+        filename = os.path.join(voxceleb_train_path,spkutt_wavfile[-1]);
+        train_dict[filename] = spk    
+    return train_dict
+
+def make_sdsv_train_dict(sdsv_train_path, sdsv_utt2spk_list):
+    with open(sdsv_utt2spk_list) as dataset_file:
+        lines = dataset_file.readlines();
+    utt2spk_dict = {}
+    for lidx, line in enumerate(lines):
+        if lidx > 0:
+            x = line.split()
+            utt2spk_dict[x[0]] = x[-1]
+
+    files = glob.glob(os.path.join(sdsv_train_path,'*/*/*.wav'));
+    train_dict = {}
+    for x in files:
+        wavfile = x.strip().split('/')[-1]
+        spk_key = wavfile.split('.')[0]
+        spk = utt2spk_dict[spk_key]
+        train_dict[x] = spk    
+    return train_dict
+
+def make_cnceleb_train_dict(cnceleb_train_path):
+    files = glob.glob(os.path.join(cnceleb_train_path,'*/*/*.flac'));
+    train_dict = {}
+    for x in files:
+        spk = x.strip().split('/')[-2]
+        train_dict[x] = spk    
+    return train_dict
+
+
+class train_dataset_loader(Dataset):
+    def __init__(self, voxceleb_train_dict, sdsv_train_dict, cnceleb_train_dict, \
+        augment_noise, musan_path, rir_path, max_frames, train_path, **kwargs):
+
+        self.augment_wav = AugmentWAV(musan_path=musan_path, rir_path=rir_path, max_frames = max_frames)
+
+        self.max_frames = max_frames;
+        self.musan_path = musan_path
+        self.rir_path   = rir_path
+        self.augment_noise    = augment_noise
+        
+        self.data_list  = []
+        self.data_label = []
+        spk_bias = 0
+
+        voxceleb_spks = list(set(voxceleb_train_dict.values()))
+        voxceleb_spks.sort()
+        voxceleb_spk_dict = {}
+        for lidx, spk_lab in enumerate(voxceleb_spks):
+            voxceleb_spk_dict[spk_lab] = lidx
+        for filename, spk in voxceleb_train_dict.items():
+            self.data_list.append(filename)
+            self.data_label.append(voxceleb_spk_dict[spk])
+        spk_bias += len(voxceleb_spks) 
+
+        sdsv_spks = list(set(sdsv_train_dict.values()))
+        sdsv_spks.sort()
+        sdsv_spk_dict = {}
+        for lidx, spk_lab in enumerate(sdsv_spks):
+            sdsv_spk_dict[spk_lab] = lidx + spk_bias
+        for filename, spk in sdsv_train_dict.items():
+            self.data_list.append(filename)
+            self.data_label.append(sdsv_spk_dict[spk])     
+        spk_bias += len(sdsv_spks)
+
+        cnceleb_spks = list(set(cnceleb_train_dict.values()))
+        cnceleb_spks.sort()
+        cnceleb_spk_dict = {}
+        for lidx, spk_lab in enumerate(cnceleb_spks):
+            cnceleb_spk_dict[spk_lab] = lidx + spk_bias
+        for filename, spk in cnceleb_train_dict.items():
+            self.data_list.append(filename)
+            self.data_label.append(cnceleb_spk_dict[spk])
+        spk_bias += len(cnceleb_spks)
+
+        self.num_label = spk_bias
+
+    def __getitem__(self, indices):
+
+        feat = []
+        #z#
+
+        for index in indices:
+            audio = loadWAV(self.data_list[index], self.max_frames)
+            
+            if self.augment_noise:
+                RIR_augtype = random.random()    
+                if RIR_augtype > 0.5:
+                    audio   = self.augment_wav.reverberate(audio)
+
+                Noise_augtype = random.random()
+                if Noise_augtype > 0.85:
+                    audio   = self.augment_wav.additive_noise('music',audio)
+                elif Noise_augtype > 0.7:
+                    audio   = self.augment_wav.additive_noise('speech',audio)
+                elif Noise_augtype > 0.5:
+                    audio   = self.augment_wav.additive_noise('noise',audio)
+                
+
+            feat.append(audio);
+
+        feat = numpy.concatenate(feat, axis=0)
+
+        return torch.FloatTensor(feat), self.data_label[index]
+
+    def __len__(self) -> int:
+        return len(self.data_list)
+
+
+class train_dataset_sampler(torch.utils.data.Sampler):
+    def __init__(self, data_source, nPerSpeaker, max_seg_per_spk, batch_size, distributed, seed, **kwargs):
+
+        self.data_label         = data_source.data_label;
+        self.nPerSpeaker        = nPerSpeaker;
+        self.max_seg_per_spk    = max_seg_per_spk;
+        self.batch_size         = batch_size;
+        self.epoch              = 0;
+        self.seed               = seed;
+        self.distributed        = distributed;
+        
+    def __iter__(self):
+
+        g = torch.Generator()
+        g.manual_seed(self.seed + self.epoch)
+        indices = torch.randperm(len(self.data_label), generator=g).tolist()
+
+        data_dict = {}
+
+        # Sort into dictionary of file indices for each ID
+        for index in indices:
+            speaker_label = self.data_label[index]
+            if not (speaker_label in data_dict):
+                data_dict[speaker_label] = [];
+            data_dict[speaker_label].append(index);
+
+
+        ## Group file indices for each class
+        dictkeys = list(data_dict.keys());
+        dictkeys.sort()
+
+        lol = lambda lst, sz: [lst[i:i+sz] for i in range(0, len(lst), sz)]
+
+        flattened_list = []
+        flattened_label = []
+        
+        for findex, key in enumerate(dictkeys):
+            data    = data_dict[key]
+            numSeg  = round_down(min(len(data),self.max_seg_per_spk),self.nPerSpeaker)
+
+            
+            rp      = lol(numpy.arange(numSeg),self.nPerSpeaker)
+            flattened_label.extend([findex] * (len(rp)))
+            for indices in rp:
+                flattened_list.append([data[i] for i in indices])
+
+        ## Mix data in random order
+        mixid           = torch.randperm(len(flattened_label), generator=g).tolist()
+        mixlabel        = []
+        mixmap          = []
+
+        ## Prevent two pairs of the same speaker in the same batch
+        for ii in mixid:
+            startbatch = round_down(len(mixlabel), self.batch_size)
+            if flattened_label[ii] not in mixlabel[startbatch:]:
+                mixlabel.append(flattened_label[ii])
+                mixmap.append(ii)
+
+        mixed_list = [flattened_list[i] for i in mixmap]
+
+        ## Divide data to each GPU
+        if self.distributed:
+            total_size  = round_down(len(mixed_list), self.batch_size * dist.get_world_size()) 
+            start_index = int ( ( dist.get_rank()     ) / dist.get_world_size() * total_size )
+            end_index   = int ( ( dist.get_rank() + 1 ) / dist.get_world_size() * total_size )
+            self.num_samples = end_index - start_index
+            return iter(mixed_list[start_index:end_index])
+        else:
+            total_size = round_down(len(mixed_list), self.batch_size)
+            self.num_samples = total_size
+            return iter(mixed_list[:total_size])
+
+    
+    def __len__(self) -> int:
+        return self.num_samples
+
+    def set_epoch(self, epoch: int) -> None:
+        self.epoch = epoch
+
+
+class test_dataset_loader(Dataset):
+    def __init__(self, test_list, test_path, **kwargs):
+        self.test_path  = test_path
+        self.test_list  = test_list
+
+    def __getitem__(self, index):
+        audio = loadWAV(os.path.join(self.test_path,self.test_list[index]))
+        return torch.FloatTensor(audio), self.test_list[index]
+
+    def __len__(self):
+        return len(self.test_list)
+
+
+class test_dataset_loader_dict(Dataset):
+    def __init__(self, test_dict, **kwargs):
+        self.test_dict  = test_dict
+        files           = self.test_dict.keys()
+        self.test_list  = list(set(files))
+        self.test_list.sort()
+
+    def __getitem__(self, index):
+        audio = loadWAV(os.path.join(self.test_dict[self.test_list[index]]))
+        return torch.FloatTensor(audio), self.test_list[index]
+
+    def __len__(self):
+        return len(self.test_list)
+
---
+++ train_select_gpu.py
@@ -0,0 +1,286 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import sys, time, os, argparse
+import yaml
+import numpy
+import torch
+import glob
+import zipfile
+import warnings
+import datetime
+from tuneThreshold import *
+from Trainer import *
+from dataloader_train_all import *
+from utils import *
+from SpeakerNet import WrappedModel, SpeakerNet
+import torch.distributed as dist
+import torch.multiprocessing as mp
+warnings.simplefilter("ignore")
+
+## ===== ===== ===== ===== ===== ===== ===== =====
+## Parse arguments
+## ===== ===== ===== ===== ===== ===== ===== =====
+
+parser = argparse.ArgumentParser(description = "SpeakerNet")
+
+parser.add_argument('--config',         type=str,   default=None,   help='Config YAML file')
+
+## Data loader
+parser.add_argument('--max_frames',     type=int,   default=200,    help='Input length to the network for training')
+parser.add_argument('--batch_size',     type=int,   default=200,    help='Batch size, number of speakers per batch')
+parser.add_argument('--max_seg_per_spk', type=int,  default=500,    help='Maximum number of utterances per speaker per epoch')
+parser.add_argument('--nDataLoaderThread', type=int, default=6,     help='Number of loader threads')
+parser.add_argument('--augment_noise',  type=bool,  default=False,  help='Augment noise and RIR to input')
+parser.add_argument('--augment_specaug',type=bool,  default=False,  help='Augment specaugmentation to input')
+parser.add_argument('--seed',           type=int,   default=10,     help='Seed for the random number generator')
+
+## Training details
+parser.add_argument('--test_interval',  type=int,   default=1,     help='Test and save every [test_interval] epochs')
+parser.add_argument('--max_epoch',      type=int,   default=80,    help='Maximum number of epochs')
+parser.add_argument('--trainfunc',      type=str,   default="",    help='Loss function')
+
+## Optimizer
+parser.add_argument('--optimizer',      type=str,   default="adam", help='sgd or adam')
+parser.add_argument('--scheduler',      type=str,   default="multisteplr", help='Learning rate scheduler')
+parser.add_argument('--lr',             type=float, default=0.001,  help='Learning rate')
+parser.add_argument("--lr_decay",       type=float, default=0.5,    help='Learning rate decay')
+parser.add_argument('--clip_grad',      type=float, default=3.0,    help="""Maximal parameter gradient norm if using gradient clipping. Clipping with norm .3 ~ 1.0 can help optimization for larger ViT architectures. 0 for disabling.""");
+parser.add_argument('--weight_decay',   type=float, default=0,      help='Weight decay in the optimizer')
+parser.add_argument("--decay_interval", type=int,   default=10,     help='Learning rate decay interval, only for [steplr] scheduler')
+parser.add_argument("--decay_epochs",   type=str,   default='[40, 45, 50, 55, 60, 65, 70, 75]', help='Learning rate decay epochs, only for [multisteplr] scheduler')
+parser.add_argument("--warmup_epoch",   type=int,   default=10,     help='Learning rate decay epochs, only for [onecyclelr] scheduler')
+
+## Loss functions
+parser.add_argument('--margin',         type=float, default=0.2,    help='Loss margin, only for some loss functions')
+parser.add_argument('--scale',          type=float, default=30,     help='Loss scale, only for some loss functions')
+parser.add_argument('--nPerSpeaker',    type=int,   default=1,      help='Number of utterances per speaker per batch, only for metric learning based losses')
+parser.add_argument('--nClasses',       type=int,   default=5994,   help='Number of speakers in the softmax layer, only for softmax-based losses')
+parser.add_argument("--hard_prob",      type=float, default=0.5,    help='Hard negative mining probability, otherwise random, only for [triplet] loss functions')
+parser.add_argument("--hard_rank",      type=int,   default=10,     help='Hard negative mining rank in the batch, only for [triplet] loss functions')
+
+## Evaluation parameters
+parser.add_argument('--dcf_p_target',   type=float, default=0.01,   help='A priori probability of the specified target speaker')
+parser.add_argument('--dcf_c_miss',     type=float, default=1,      help='Cost of a missed detection')
+parser.add_argument('--dcf_c_fa',       type=float, default=1,      help='Cost of a spurious detection')
+
+## Load and save
+parser.add_argument('--initial_model',  type=str,   default="",     help='Initial model weights')
+parser.add_argument('--save_path',      type=str,   default="exps/exp1", help='Path for model and logs')
+
+## Training and test data
+# parser.add_argument('--train_list',     type=str,   default="data/train_list.txt",  help='Train list')
+# parser.add_argument('--test_list',      type=str,   default="data/test_list.txt",   help='Evaluation list')
+# parser.add_argument('--train_path',     type=str,   default="data/voxceleb2", help='Absolute path to the train set')
+# parser.add_argument('--test_path',      type=str,   default="data/voxceleb1", help='Absolute path to the test set')
+# parser.add_argument('--musan_path',     type=str,   default="data/musan_split", help='Absolute path to the test set')
+# parser.add_argument('--rir_path',       type=str,   default="data/RIRS_NOISES/simulated_rirs", help='Absolute path to the test set')
+    ########## real example (Choi Jeong-Hwan) ########### 
+parser.add_argument('--train_list',        type=str,   default="/home/jh2/Workspace/cjh/fire/sess_torch/meta_vc2_mfbe80/voxceleb2/all/wav_vc2_train.scp",           help='Path for Vox_2 dev list')
+parser.add_argument('--test_list',         type=str,   default="/home/jh2/Workspace/cjh/fire/sess_torch/meta_vc1/veri_test_clean.txt",     help='Evaluation list');
+parser.add_argument('--train_path',        type=str,   default="",          help='Absolute path to the train set');
+parser.add_argument('--test_path',         type=str,   default="/media/jh2/f22b587f-8065-4c02-9b74-f6b9f5a89581/DB/VoxCeleb1/test/wav/", help='Absolute path to the test set');
+parser.add_argument('--musan_path',        type=str,   default="/media/jh2/f22b587f-8065-4c02-9b74-f6b9f5a89581/DB/musan_split", help='Absolute path to the test set');
+parser.add_argument('--rir_path',          type=str,   default="/media/jh2/f22b587f-8065-4c02-9b74-f6b9f5a89581/DB/RIRS_NOISES/simulated_rirs", help='Absolute path to the test set');
+
+parser.add_argument('--sdsv_train_path',        type=str,   default="/media/jh2/f22b587f-8065-4c02-9b74-f6b9f5a89581/DB/SDSV21_challenge",           help='')
+parser.add_argument('--sdsv_utt2spk_list',      type=str,   default="/media/jh2/f22b587f-8065-4c02-9b74-f6b9f5a89581/DB/SDSV21_challenge/docs/train_labels.txt",          help='')
+parser.add_argument('--cnceleb_train_path',     type=str,   default="/media/jh2/f22b587f-8065-4c02-9b74-f6b9f5a89581/DB/CN-Celeb_flac",           help='')
+
+
+## Model definition
+parser.add_argument('--n_mels',         type=int,   default=40,     help='Number of mel filterbanks')
+parser.add_argument('--log_input',      type=bool,  default=True,  help='Log input features')
+parser.add_argument('--model',          type=str,   default="",     help='Name of model definition')
+parser.add_argument('--encoder_type',   type=str,   default="SAP",  help='Type of encoder')
+parser.add_argument('--nOut',           type=int,   default=512,    help='Embedding size in the last FC layer')
+parser.add_argument('--sinc_stride',    type=int,   default=10,    help='Stride size of the first analytic filterbank layer of RawNet3')
+parser.add_argument('--gpu_id',         type=str,   default="0",    help='Select GPU')
+
+## Distributed and mixed precision training
+parser.add_argument('--port',           type=str,   default="8888", help='Port for distributed training, input as text')
+parser.add_argument('--distributed',    dest='distributed', action='store_true', help='Enable distributed training')
+parser.add_argument('--mixedprec',      dest='mixedprec',   action='store_true', help='Enable mixed precision training')
+
+args = parser.parse_args()
+
+## Parse YAML
+def find_option_type(key, parser):
+    for opt in parser._get_optional_actions():
+        if ('--' + key) in opt.option_strings:
+           return opt.type
+    raise ValueError
+
+if args.config is not None:
+    with open(args.config, "r") as f:
+        yml_config = yaml.load(f, Loader=yaml.FullLoader)
+    for k, v in yml_config.items():
+        if k in args.__dict__:
+            typ = find_option_type(k, parser)
+            args.__dict__[k] = typ(v)
+        else:
+            sys.stderr.write("Ignored unknown parameter {} in yaml.\n".format(k))
+
+
+## ===== ===== ===== ===== ===== ===== ===== =====
+## Trainer script
+## ===== ===== ===== ===== ===== ===== ===== =====
+
+def main_worker(gpu, ngpus_per_node, args):
+
+
+    ## Load models
+    s = SpeakerNet(**vars(args))
+
+    if args.distributed:
+        os.environ['MASTER_ADDR']='localhost'
+        os.environ['MASTER_PORT']=args.port
+
+        dist.init_process_group(backend='nccl', world_size=ngpus_per_node, rank=args.gpu)
+
+        torch.cuda.set_device(args.gpu)
+        s.cuda(args.gpu)
+
+        s = torch.nn.parallel.DistributedDataParallel(s, device_ids=[args.gpu], find_unused_parameters=True)
+
+        print('Loaded the model on GPU {:d}'.format(args.gpu))
+
+    else:
+        s = WrappedModel(s).cuda()
+
+    it = 1
+    eers = [100]
+
+    args.gpu = args.gpu_id
+
+    if args.gpu == args.gpu_id:
+        ## Write args to scorefile
+        scorefile   = open(args.result_save_path+"/scores.txt", "a+")
+        dict2scp(args.result_save_path+"/args.scp", vars(args))
+
+    ## Initialise trainer and data loader
+
+    vox_train_dict = make_voxceleb_train_dict(args.train_list, args.train_path)
+    sdsv_train_dict = make_sdsv_train_dict(args.sdsv_train_path, args.sdsv_utt2spk_list)
+    cn_train_dict = make_cnceleb_train_dict(args.cnceleb_train_path)
+
+    train_dataset = train_dataset_loader(vox_train_dict, sdsv_train_dict, cn_train_dict, **vars(args))
+    print('#Class :',train_dataset.num_label)
+
+    train_sampler = train_dataset_sampler(train_dataset, **vars(args))
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        num_workers=args.nDataLoaderThread,
+        sampler=train_sampler,
+        pin_memory=False,
+        worker_init_fn=worker_init_fn,
+        drop_last=True,
+    )
+
+    if args.scheduler == 'onecyclelr':
+        epoch_per_sample = []
+        for _ in range(1):
+            iter(train_sampler)
+            epoch_per_sample.append(len(train_sampler))
+        args.epoch_per_sample = max(epoch_per_sample)
+    
+    trainer     = ModelTrainer(s, **vars(args))
+
+    ## Load model weights
+    modelfiles = glob.glob('%s/model0*.model'%args.model_save_path)
+    modelfiles.sort()
+
+    if(args.initial_model != ""):
+        trainer.loadParameters(args.initial_model)
+        print("Model {} loaded!".format(args.initial_model))
+    elif len(modelfiles) >= 1:
+        trainer.loadParameters(modelfiles[-1])
+        print("Model {} loaded from previous state!".format(modelfiles[-1]))
+        it = int(os.path.splitext(os.path.basename(modelfiles[-1]))[0][5:]) + 1
+
+    for ii in range(1,it):
+        trainer.__scheduler__.step()
+
+    ## Save training code and params
+    if args.gpu == args.gpu_id:
+        pyfiles = glob.glob('./*.py')
+        strtime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+        zipf = zipfile.ZipFile(args.result_save_path+ '/run%s.zip'%strtime, 'w', zipfile.ZIP_DEFLATED)
+        for file in pyfiles:
+            zipf.write(file)
+        zipf.close()
+
+        with open(args.result_save_path + '/run%s.cmd'%strtime, 'w') as f:
+            f.write('%s'%args)
+
+    ## Core training script
+    for it in range(it,args.max_epoch+1):
+
+        train_sampler.set_epoch(it)
+
+        clr = [x['lr'] for x in trainer.__optimizer__.param_groups]
+
+        loss, traineer = trainer.train_network(train_loader, verbose=(args.gpu == args.gpu_id))
+
+        if args.gpu == args.gpu_id:
+            print('\n',time.strftime("%Y-%m-%d %H:%M:%S"), "Epoch {:d}, TEER/TAcc {:2.2f}, TLOSS {:f}, LR {:f}".format(it, traineer, loss, max(clr)))
+            scorefile.write("Epoch {:d}, TEER/TAcc {:2.2f}, TLOSS {:f}, LR {:f} \n".format(it, traineer, loss, max(clr)))
+
+        if it % args.test_interval == 0:
+
+            sc, lab, _ = trainer.evaluateFromList(**vars(args))
+
+            if args.gpu == args.gpu_id:
+                
+                result = tuneThresholdfromScore(sc, lab, [1, 0.1])
+
+                fnrs, fprs, thresholds = ComputeErrorRates(sc, lab)
+                mindcf, threshold = ComputeMinDcf(fnrs, fprs, thresholds, args.dcf_p_target, args.dcf_c_miss, args.dcf_c_fa)
+
+                eers.append(result[1])
+
+                print('\n',time.strftime("%Y-%m-%d %H:%M:%S"), "Epoch {:d}, VEER {:2.4f}, MinDCF {:2.5f}".format(it, result[1], mindcf))
+                scorefile.write("Epoch {:d}, VEER {:2.4f}, MinDCF {:2.5f}\n".format(it, result[1], mindcf))
+
+                trainer.saveParameters(args.model_save_path+"/model%09d.model"%it)
+
+                with open(args.model_save_path+"/model%09d.eer"%it, 'w') as eerfile:
+                    eerfile.write('{:2.4f}'.format(result[1]))
+
+                scorefile.flush()
+
+    if args.gpu == args.gpu_id:
+        scorefile.close()
+
+
+## ===== ===== ===== ===== ===== ===== ===== =====
+## Main function
+## ===== ===== ===== ===== ===== ===== ===== =====
+
+def main():
+    args.model_save_path     = args.save_path+"/model"
+    args.result_save_path    = args.save_path+"/result"
+    args.feat_save_path      = ""
+
+    os.makedirs(args.model_save_path, exist_ok=True)
+    os.makedirs(args.result_save_path, exist_ok=True)
+
+    # n_gpus = torch.cuda.device_count()
+    os.environ["CUDA_VISIBLE_DEVICES"]=args.gpu_id
+
+    print('Python Version:', sys.version)
+    print('PyTorch Version:', torch.__version__)
+    print('Number of GPUs:', torch.cuda.device_count())
+    print('Save path:',args.save_path)
+
+    if args.distributed:
+        mp.spawn(main_worker, nprocs=n_gpus, args=(n_gpus, args))
+    else:
+        main_worker(0, None, args)
+
+
+if __name__ == '__main__':
+    main()
